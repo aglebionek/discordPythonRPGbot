970b88ca73147963a0cd4804cec1a51572f0f80c
aglebionek <aglebionek@gmail.com>
2021-10-03 21:13:28 +0200

    replace if block with a function dict

--- BotClient.py
+++ BotClient.py
@@ -1,33 +1,18 @@
 import discord
-from BotFunctions import BotFunctions
+import re
+
+
 import BotCommands
+from bot_functions import BotFunctions
 import BotOptions
 import my_token
-import re
+
          
 class BotClient(discord.Client):
-    '''
-    #region --- read options from BotOptions.py ---
-    #
-    #
     
-    # command symbol is the sign you put at the start of a command, to let the bot knows you want it to do something
-    command_symbol = BotOptions.COMMAND_SYMBOL
-    
-    # defines the language of the bot's responses, based in languages/channel_messages/language.json
-    message_language = BotOptions.MESSAGE_LANGUAGE
-    
-    # defines the languages to use for character info, based in languages/character_info/language.json
-    character_languages = BotOptions.CHARACTER_LANGUAGES
-    
-    #
-    #
-    #endregion end --- read options from BotOptions.py ---
-    '''
-   
     async def on_ready(self):
         game = discord.Game(name = "Call of Cthulhu RPG session")
-        await client.change_presence(activity=game)
+        await client.change_presence(activity=game)        
         print('Logged in as')
         print(self.user.name)
         print(self.user.id)
@@ -40,114 +25,33 @@ class BotClient(discord.Client):
         if message.author.id == self.user.id:
             return
 
-        # extract the content string from discord.Message object, make it lower case and mark it as a string
+        # extract message content and channel from the message
         content: str = message.content.lower()
-        
-        # extract the channel from discord.Message object and mark it as a discord.TextChannel object
         channel: discord.TextChannel = message.channel
         
-        # check if the message content is a command (it is if it starts with the command symbol)
-        if content.startswith(BotOptions.COMMAND_PREFIX):
-            
-            # remove the command symbol from the content string
+        if channel.name in BotOptions.CHARACTER_CHANNELS:
+            if message.attachments:
+                if [0].filename.endswith(".json"):
+                    await channel.send("gocha")
+        
+        if content.startswith(BotOptions.COMMAND_PREFIX):           
             content = content[len(BotOptions.COMMAND_PREFIX):]
             
-            # define a match variable
-            match = None
-        
-            #region --- check for dice_roll_1 command ---
-            #
-            #
+            matches = list()
             
-            # check if the message's content matches the command's regex
-            match = re.match(BotCommands.DICE_ROLL_1, content)
-            
-            # if message matched the command's regex
-            if match is not None: 
+            for command_name, command_re in BotCommands.commands.items():
+                match = re.match(command_re, content)
+                if match is not None: 
+                    matches.append((command_name, match))
                 
-                # extract the size of dice from the regex
-                dice_size = int(match.group(1))
-                
-                # set default number of dice to one
-                dice_number = 1
-                
-                # if number of dice was specified
-                if match.group(2) is not None:
-                    
-                    # extract the number of dice from the regex
-                    dice_number = int(match.group(3))
-                
-                # roll the dice and send the returned formatted string as a message to the channel
-                await channel.send(await BotFunctions.dice_roll(dice_size, dice_number))
-                return
-            
-            #
-            #
-            #endregion --- check for dice_roll_1 command ---
-            
-            #region --- check for dice_roll_2 command ---
-            #
-            #
-            
-            # check if the message's content matches the command's regex
-            match = re.match(BotCommands.DICE_ROLL_2, content)
-            
-            # if message matched the command's regex
-            if match is not None:
-                
-                # extract the number of dice from the regex            
-                dice_number = int(match.group(1))
-                
-                # extract the size of dice from the regex
-                dice_size = int(match.group(2))
-                
-                # roll the dice and send the returned formatted string as a message to the channel
-                await channel.send(await BotFunctions.dice_roll(dice_size, dice_number))
-                return
-            
-            #
-            #
-            #endregion --- check for dice_roll_2 command ---
-                
-            #region -- check for help_command ---
-            #
-            #
-            
-            # check if the message's content matches the command's regex
-            match = re.match(BotCommands.HELP_COMMAND, content)
-            
-            # if message matched the command's regex
-            if match is not None:
-                
-                # send the returned list of commands as a message to the channel
-                await channel.send(await BotFunctions.help())
-                return
-            
-            #
-            #
-            #endregion -- check for help_command ---
-            
-            #region --- check for skill_test command ---
-            #
-            #
-            
-            # check if the message's content matches the command's regex
-            match = re.match(BotCommands.SKILL_TEST, content)
-            
-            # if message matched the command's regex
-            if match is not None:
-                
-                # extract the skill to test from the regex
-                skill = match.group(2)
-                
-                # send the returned formatted string as a message to the channel
-                await channel.send(await BotFunctions.test(skill))
-                return 
-            
-            #
-            #
-            #endregion --- check for skill_test command ---
-            
+            if len(matches) == 0:
+                await channel.send(f"I'm sorry, command '{content}' doesn't match any of the commands.")
+            elif len(matches) > 1:
+                await channel.send(f"There are more than one matches found, please choose from {matches}.")
+            else:
+                command_name, match = matches[0]
+                await channel.send(await vars(BotFunctions)[command_name](match))                
+
 if __name__ == "__main__":
     client = BotClient()
     current_token = my_token.Token()
--- BotOptions.py
+++ BotOptions.py
@@ -1,6 +1,7 @@
 COMMAND_PREFIX = ''
 MESSAGE_LANGUAGE = 'eng'
 CHARACTER_LANGUAGES = ['eng', 'pl']
+CHARACTER_CHANNELS = ['materials', 'hidden-channel']
 
 # to implement in the future
 #COMMAND_LANGUAGE = 'eng'
